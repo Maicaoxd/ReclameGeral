511e00cec7595919c6428d38ce2608ce56167b83
Maicon Guedes <maicon.ag17@hotmail.com>
2024-03-24 22:52:25 -0300

    Avaliacao, Categoria e Empresa

--- src/main/java/com/reclamegeral/dao/EmpresaDAO.java
+++ src/main/java/com/reclamegeral/dao/EmpresaDAO.java
@@ -2,44 +2,78 @@ package com.reclamegeral.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 import com.reclamegeral.model.Empresa;
+import com.reclamegeral.util.JPAUtil;
 
 public class EmpresaDAO implements IEmpresaDAO {
 
+	EntityManager em = JPAUtil.getEntityManager();
+
 	@Override
 	public void salvar(Empresa empresa) {
-		// TODO Auto-generated method stub
-
+		EntityTransaction transaction = em.getTransaction();
+		try {
+			transaction.begin();
+			em.persist(empresa);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public Empresa buscarPorId(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.find(Empresa.class, id);
 	}
 
 	@Override
 	public void atualizar(Empresa empresa) {
-		// TODO Auto-generated method stub
-
+		EntityTransaction transaction = em.getTransaction();
+		try {
+			transaction.begin();
+			em.merge(empresa);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void remover(Empresa empresa) {
-		// TODO Auto-generated method stub
-
+		EntityTransaction transaction = em.getTransaction();
+		try {
+			transaction.begin();
+			em.remove(em.contains(empresa) ? empresa : em.merge(empresa));
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public List<Empresa> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("SELECT e FROM Empresa e", Empresa.class).getResultList();
 	}
 
 	@Override
 	public void fecharConexao() {
-		// TODO Auto-generated method stub
-
+		if (em != null) {
+			em.close();
+		}
+		if (em != null) {
+			em.close();
+		}
 	}
-
 }
